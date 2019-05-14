78e4db53df3250261b09a5867bfc6489d2a15ef9
Sunil Pashikanti <Sunil.Pashikanti@hitachiconsulting.com>
2019-05-14 23:44:16 +0530

    implemented autocomplete functionality, integrated with stored procedure

--- MyReview/Controllers/ReviewController.cs
+++ MyReview/Controllers/ReviewController.cs
@@ -1,6 +1,8 @@
 ﻿using MyReview.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,26 +17,27 @@ namespace MyReview.Controllers
             return View();
         }
         [HttpPost]
-        public JsonResult Index(string Prefix)
+        public JsonResult Index(string keyword)
         {
             //Note : you can bind same list from database  
-            List<SubCategoryModel> ObjList = new List<SubCategoryModel>()
-            {
+            List<SubCategoryModel> ObjList = GetSearchItems(keyword);
+                //new List<SubCategoryModel>()
+                //    {
 
-                new SubCategoryModel {SubCategoryName="Latur",Description="Latur City is good" },
-                new SubCategoryModel {SubCategoryName="Mumbai",Description="Mumbai city is huge" },
-                new SubCategoryModel {SubCategoryName="Pune" ,Description="Pune city is cool" },
-                new SubCategoryModel {SubCategoryName="Delhi",Description="Delhi city is crowdy" },
-                new SubCategoryModel {SubCategoryName="Dehradun",Description="Dehradun city is pleasant" },
-                new SubCategoryModel {SubCategoryName="Noida",Description="Noida city is creepy" },
-                new SubCategoryModel {SubCategoryName="New Delhi",Description="New Delhi city is capital" }
+            //        new SubCategoryModel {SubCategoryName="Latur",Description="Latur City is good" },
+            //        new SubCategoryModel {SubCategoryName="Mumbai",Description="Mumbai city is huge" },
+            //        new SubCategoryModel {SubCategoryName="Pune" ,Description="Pune city is cool" },
+            //        new SubCategoryModel {SubCategoryName="Delhi",Description="Delhi city is crowdy" },
+            //        new SubCategoryModel {SubCategoryName="Dehradun",Description="Dehradun city is pleasant" },
+            //        new SubCategoryModel {SubCategoryName="Noida",Description="Noida city is creepy" },
+            //        new SubCategoryModel {SubCategoryName="New Delhi",Description="New Delhi city is capital" }
 
-        };
+            //};
             //Searching records from list using LINQ query  
-            var CityList = (from N in ObjList
-                            where N.SubCategoryName.StartsWith(Prefix)
-                            select new { N.SubCategoryName });
-            return Json(CityList, JsonRequestBehavior.AllowGet);
+            //var CityList = (from N in ObjList
+            //                where N.SubCategoryName.StartsWith(Prefix)
+            //                select new { N.SubCategoryName });
+            return Json(ObjList, JsonRequestBehavior.AllowGet);
         }
         public ActionResult WriteReview()
         {
@@ -47,6 +50,42 @@ namespace MyReview.Controllers
         public ActionResult ReadMore()
         {
             return View();
+        }
+
+        private List<SubCategoryModel> GetSearchItems(string keyword)
+        {
+            List<SubCategoryModel> items = new List<SubCategoryModel>();
+            string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                string query = "SearchProducts";
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlParameter prodname = new SqlParameter("@prodname", keyword);
+                    cmd.Parameters.Add(prodname);
+
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            items.Add(new SubCategoryModel
+                            {
+                                SubCategoryId = sdr["SubCategoryId"].ToString(),
+                                SubCategoryName = sdr["SubCategoryName"].ToString(),
+                                Description = sdr["Description"].ToString(),
+                                ImageName = sdr["ImageName"].ToString(),
+                                CategoryId = sdr["CategoryId"].ToString()
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+
+            return items;
         }
     }
 }
